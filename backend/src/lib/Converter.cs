72a81bfb006bd372f5995cc45e2e08beefee9e9e
omlnaut <oneironaut.oml@gmail.com>
2026-06-12 20:25:32 +0000

    sub 100 conversions

--- backend/src/lib/Converter.cs
+++ backend/src/lib/Converter.cs
@@ -2,11 +2,64 @@ namespace backend.lib;
 
 public static class Converter
 {
-	public static string ToCurrency(decimal number)
-	{
-		// validate
-		// - range
-		// - either 0 or 2 decimal digits
-		return "";
-	}
+    public static string ToCurrency(decimal number)
+    {
+        // validate
+        // - range
+        // - either 0 or 2 decimal digits
+        var hundreds = (int)number % 100;
+        var converted = ConvertBelow100(hundreds);
+        return converted + (number == 1 ? " dollar" : " dollars");
+    }
+
+    private static string ConvertBelow100(int number)
+    {
+        if (number < 20)
+            return SpecialNumbers[number];
+
+        var (ten, one) = Math.DivRem(number, 10);
+        var tenStr = Tens[ten];
+        if (one == 0)
+            return tenStr;
+
+        var oneStr = SpecialNumbers[one];
+        return $"{tenStr}-{oneStr}";
+    }
+
+    private static string[] SpecialNumbers =>
+        [
+            "zero",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+            "ten",
+            "eleven",
+            "twelve",
+            "thirteen",
+            "fourteen",
+            "fifteen",
+            "sixteen",
+            "seventeen",
+            "eighteen",
+            "nineteen",
+        ];
+
+    private static Dictionary<int, string> Tens =>
+        new()
+        {
+            { 2, "twenty" },
+            { 3, "thirty" },
+            { 4, "fourty" },
+            { 5, "fifty" },
+            { 6, "sixty" },
+            { 7, "seventy" },
+            { 8, "eighty" },
+            { 9, "ninety" },
+        };
 }
--- backend/tests/BackendTests/ConversionTests.cs
+++ backend/tests/BackendTests/ConversionTests.cs
@@ -15,15 +15,32 @@ public class Tests
         Assert.That(Converter.ToCurrency(number), Is.EqualTo(target));
     }
 
+    [TestCase(20, "twenty dollars")]
+    [TestCase(21, "twenty-one dollars")]
+    [TestCase(32, "thirty-two dollars")]
+    [TestCase(43, "fourty-three dollars")]
+    [TestCase(54, "fifty-four dollars")]
+    [TestCase(65, "sixty-five dollars")]
+    [TestCase(76, "seventy-six dollars")]
+    [TestCase(87, "eighty-seven dollars")]
+    [TestCase(98, "ninety-eight dollars")]
+    [TestCase(99, "ninety-nine dollars")]
+    public void Sub100Conversions(decimal number, string target)
+    {
+        Assert.That(Converter.ToCurrency(number), Is.EqualTo(target));
+    }
+
     [TestCase(0, "zero dollars")]
     [TestCase(1, "one dollar")]
     [TestCase(25.1, "twenty-five dollars and ten cents")]
     [TestCase(0.01, "zero dollars and one cent")]
     [TestCase(45_100, "forty - five thousand one hundred dollars")]
-    [TestCase(999_999_999.99, "nine hundred ninety - nine million nine hundred ninety -nine thousand nine hundred")]
+    [TestCase(
+        999_999_999.99,
+        "nine hundred ninety - nine million nine hundred ninety -nine thousand nine hundred"
+    )]
     public void ExamplesFromTask(decimal number, string target)
     {
         Assert.That(Converter.ToCurrency(number), Is.EqualTo(target));
     }
-
 }
