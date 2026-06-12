fe6c705cdeb3dcd3b09d75294c20ef64bba1f3cc
omlnaut <oneironaut.oml@gmail.com>
2026-06-12 14:17:31 +0000

    app to api smoketest

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div>testi</div>;
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    async function callApi() {
+      const preResponse = await fetch("http://localhost:5196/weatherforecast");
+      const response = await preResponse.json();
+
+      setResponse(JSON.stringify(response));
+    }
+    callApi();
+  }, []);
+  return <div>Api response: {response}</div>;
 }
 
 export default App;
