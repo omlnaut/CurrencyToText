2b197c07b956786948fd7d3215411c1942162067
omlnaut <oneironaut.oml@gmail.com>
2026-06-14 19:28:45 +0000

    endpoint: lanuage choice

--- backend/src/ConversionController.cs
+++ backend/src/ConversionController.cs
@@ -3,15 +3,27 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace backend;
 
+public enum ConversionLanguage
+{
+    ENGLISH,
+    GERMAN,
+}
+
 public record ConversionResponse(string ConvertedNumber);
 
 [ApiController]
 public class ConversionController : ControllerBase
 {
     [HttpGet("Convert")]
-    public ActionResult<ConversionResponse> Convert(decimal number)
+    public ActionResult<ConversionResponse> Convert(decimal number, ConversionLanguage language)
     {
-        var words = EnglishConverter.ToCurrency(number);
+        var words = language switch
+        {
+            ConversionLanguage.ENGLISH => EnglishConverter.ToCurrency(number),
+            ConversionLanguage.GERMAN => GermanConverter.ToCurrency(number),
+            _ => throw new NotImplementedException(),
+        };
+
         return new ConversionResponse(words);
     }
 }
